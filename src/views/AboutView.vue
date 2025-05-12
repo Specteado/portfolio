42bd333ba842432e3463afeb040d55b52bd2f3ef
Specteado <101020829+Specteado@users.noreply.github.com>
2025-05-12 02:12:11 +0200

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -107,21 +107,18 @@ export default {
   }
 }
 </script>
-<template>
-  <div
-    class="bg-[#1e1e1f] px-5 py-5 md:px-12 md:py-10 text-left border border-[#383838] rounded-3xl text-amber-50 mx-3 mb-5">
-    <article data-page="about">
 
+<template>
+  <div class="bg-[#1e1e1f] px-5 py-5 md:px-12 md:py-10 text-left border border-[#383838] rounded-3xl text-red-100 mx-3 mb-5">
+    <article data-page="about">
       <header>
         <div class="text-2xl font-bold text-white mb-5 fadein-bot title-section flex items-center">
           About Me &nbsp;
-          <div class="h-[1px] w-32 bg-amber-200 md:w-96 aos-init aos-animate" data-aos="zoom-in-left"
-            data-aos-duration="600"></div>
+          <div class="h-[1px] w-32 bg-red-400 md:w-96 aos-init aos-animate" data-aos="zoom-in-left" data-aos-duration="600"></div>
         </div>
       </header>
 
-      <section
-        class="text-sm md:text-lg text-justify flex flex-col gap-4 md:flex-row md:gap-8 md:justify-left md:items-center">
+      <section class="text-sm md:text-lg text-justify flex flex-col gap-4 md:flex-row md:gap-8 md:justify-left md:items-center">
         <div class="flex justify-center">
           <img class="w-9/12 rounded-full mb-3 fadein-up"
             src="https://media.discordapp.net/attachments/1366212695455432734/1371270527976476722/2222.gif?ex=682286b2&is=68213532&hm=f05daf6f0ec6675d48cf6c31fe558da63954089ede751527728f348b3089cd79&=" alt="Foto">
@@ -131,82 +128,73 @@ export default {
             &nbsp; &nbsp; &nbsp; ¡Hola, buenas! Me llamo Jusef Masabanda, más conocido como Specteado. Soy programador en el
             ámbito de FiveM, Node.js, LUA, Editor y JavaScript.
             Tengo 4 años de experiencia como programador, y actualmente me estoy formando en Ciberseguridad,
-            trabajando con máquinas
-            Linux a través de DockerLabs.
+            trabajando con máquinas Linux a través de DockerLabs.
           </p>
           <p class="mb-3 fadein-left fadeins-2">
             &nbsp; &nbsp; &nbsp; Actualmente estoy trabajando en la creación de una página web con Vue para probar distintas
             funcionalidades. También he estado programando bots de Discord utilizando las librerías de Node.js y
             explotando máquinas en DockerLabs, lo cual me resulta muy fácil.
             Mi objetivo en este momento es ser contratado por una empresa y adquirir más conocimientos en
-            nformática para enriquecer mi experiencia. Si estás interesado, puedes contactarme por medio de mis redes sociales.
+            informática para enriquecer mi experiencia. Si estás interesado, puedes contactarme por medio de mis redes sociales.
           </p>
         </div>
-
       </section>
-
     </article>
   </div>
 
-  <div class="px-5 py-5 md:px-12 md:py-10 text-left text-amber-50 mx-3">
+  <div class="px-5 py-5 md:px-12 md:py-10 text-left text-red-100 mx-3">
     <article data-page="about">
       <header>
         <div class="text-2xl font-bold text-white mb-5 fadein-bot title-section flex items-center">
-          <div class="h-[1px] w-10 bg-amber-200 md:w-20 aos-init aos-animate" data-aos="zoom-in-left"
-            data-aos-duration="600"></div>
+          <div class="h-[1px] w-10 bg-red-400 md:w-20 aos-init aos-animate" data-aos="zoom-in-left" data-aos-duration="600"></div>
           &nbsp; Skills
         </div>
       </header>
+
       <section>
         <div>
           <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 mb-5">
             <li class="mr-2">
               <button class="inline-block px-4 py-3 rounded-lg hover:text-white"
-                :class="{ 'text-amber-200 bg-amber-200 bg-opacity-10': activeTab === 1 }" @click="activeTab = 1">Tech Stack</button>
+                :class="{ 'text-red-400 bg-red-400 bg-opacity-10': activeTab === 1 }" @click="activeTab = 1">Tech Stack</button>
             </li>
             <li class="mr-2">
               <button class="inline-block px-4 py-3 rounded-lg hover:text-white"
-                :class="{ 'text-amber-200 bg-amber-200 bg-opacity-10': activeTab === 2 }" @click="activeTab = 2">Tools</button>
+                :class="{ 'text-red-400 bg-red-400 bg-opacity-10': activeTab === 2 }" @click="activeTab = 2">Tools</button>
             </li>
           </ul>
         </div>
+
         <div v-show="activeTab === 1">
           <div class="grid grid-cols-2 gap-4 pb-32 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12">
             <div v-for="item in tech" :key="item.id">
-              <div
-                class="item-tech flex cursor-pointer items-center gap-2 rounded border border-amber-200 px-2 py-2 hover:bg-amber-200 hover:bg-opacity-10 md:gap-3 lg:px-3">
+              <div class="item-tech flex cursor-pointer items-center gap-2 rounded border border-red-400 px-2 py-2 hover:bg-red-400 hover:bg-opacity-10 md:gap-3 lg:px-3">
                 <div class="flex h-12 w-12 items-center justify-center p-0 lg:h-16 lg:w-16 lg:p-2 zoom-in">
                   <img alt="HTML" loading="lazy" width="32" height="32" decoding="async" data-nimg="1"
-                    class="img-tech drop-shadow-xl transition-all duration-300 h-[65%] w-[65%] lg:h-[85%] lg:w-[85%]  "
+                    class="img-tech drop-shadow-xl transition-all duration-300 h-[65%] w-[65%] lg:h-[85%] lg:w-[85%]"
                     :src="item.imageUrl" style="color: transparent;">
                 </div>
                 <div class="flex items-center text-sm md:text-base lg:text-lg">
-                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name }}
-                  </div>
-                  <div
-                    class="status-tech opacity-0 absolute mt-5 text-[10px] text-amber-200 transition-all duration-300 md:text-xs lg:text-sm">
-                    {{ item.status }}</div>
+                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name }}</div>
+                  <div class="status-tech opacity-0 absolute mt-5 text-[10px] text-red-400 transition-all duration-300 md:text-xs lg:text-sm">{{ item.status }}</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
         <div v-show="activeTab === 2">
           <div class="grid grid-cols-2 gap-4 pb-32 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12">
             <div v-for="item in tools" :key="item.id">
-              <div
-                class="item-tech flex cursor-pointer items-center gap-2 rounded border border-amber-200 px-2 py-2 hover:bg-amber-200 hover:bg-opacity-10 md:gap-3 lg:px-3">
+              <div class="item-tech flex cursor-pointer items-center gap-2 rounded border border-red-400 px-2 py-2 hover:bg-red-400 hover:bg-opacity-10 md:gap-3 lg:px-3">
                 <div class="flex h-12 w-12 items-center justify-center p-0 lg:h-16 lg:w-16 lg:p-2 zoom-in">
                   <img alt="HTML" loading="lazy" width="32" height="32" decoding="async" data-nimg="1"
-                    class="img-tech drop-shadow-xl transition-all duration-300 h-[65%] w-[65%] lg:h-[85%] lg:w-[85%]  "
+                    class="img-tech drop-shadow-xl transition-all duration-300 h-[65%] w-[65%] lg:h-[85%] lg:w-[85%]"
                     :src="item.imageUrl" style="color: transparent;">
                 </div>
                 <div class="flex items-center text-sm md:text-base lg:text-lg">
-                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name }}
-                  </div>
-                  <div
-                    class="status-tech opacity-0 absolute mt-5 text-[10px] text-amber-200 transition-all duration-300 md:text-xs lg:text-sm">
-                    {{ item.status }}</div>
+                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name }}</div>
+                  <div class="status-tech opacity-0 absolute mt-5 text-[10px] text-red-400 transition-all duration-300 md:text-xs lg:text-sm">{{ item.status }}</div>
                 </div>
               </div>
             </div>
